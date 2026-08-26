92b4ce671350cefe7edddc1d7e30afd3afb4c719
28LPotter <28LPotter@live.ytech.edu>
2026-08-26 12:00:03 -0400

    more progress on the calculator

--- docpac_022808/JavaScript Calculator/PotterLogan/Calculcate.js
+++ docpac_022808/JavaScript Calculator/PotterLogan/Calculcate.js
@@ -1,7 +1,27 @@
 function Calculate(){
 
-num1 = Number(document.getElementById("num1"))
-num1 = Number(document.getElementById("num2"))
+num1 = Number(document.getElementById("num1").V)
+num2 = Number(document.getElementById(num2))
+operation =(document.getElementById(operation))
 
+switch(String(operation)){
+
+
+    case "*":
+        output = num1 * num2
+        break;
+    case "/":
+        if(num2 != 0){ output = num1 / num2 } else output = "Error!"
+        break;
+    case "+":
+        output = num1 + num2
+        break;
+    case "-":
+        output = num1 - num2        
+}
+
+//document.getElementById("result").textContent = String(output)
+
+console.log("num1=" + num1 + " num2=" + num2 + " operation=" + operation + " output="/* + output*/)
 
 }
